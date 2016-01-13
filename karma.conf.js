3a2df79189f88941f8d9b929d7f72ecd3d21b283
Marco Patino <marco.patino@unosquare.com>
2016-01-13 16:35:04 -0600

    Integrated karma-systemjs

--- karma.conf.js
+++ karma.conf.js
@@ -1,77 +1,105 @@
 // Karma configuration
-module.exports = function (config) {
-    config.set({
+// Generated on Wed Jan 13 2016 12:19:41 GMT-0600 (CST)
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
+module.exports = function(config) {
+  config.set({
 
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
 
-        // list of files / patterns to load in the browser
-        // list of files / patterns to load in the browser
-        files: [
-            'node_modules/traceur/bin/traceur-runtime.js',
-            'node_modules/angular2/bundles/angular2-polyfills.js',
-            'node_modules/systemjs/dist/system.js',
-            'node_modules/rxjs/bundles/Rx.js',
-            'node_modules/angular2/bundles/angular2.dev.js',
-            { pattern: 'dist/app/**/*.js', included: false, serve: true, watched: false },
-            { pattern: 'dist/app/**/*.spec.js', included: true, serve: true, watched: true },
-            'test-main.js'
-        ],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['systemjs', 'jasmine'],
 
 
-        // list of files to exclude
-        exclude: [
-            'dist/bootstrap.js'
-        ],
-
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-        },
-
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'html'],
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/traceur/bin/traceur-runtime.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/angular2/bundles/angular2.dev.js',
+      //{ pattern: 'dist/app/**/*.js', included: false, serve: true, watched: false },
+      //{ pattern: 'dist/app/**/*.spec.js', included: true, serve: true, watched: true }
+    ],
 
 
-        // web server port
-        port: 9876,
+    // list of files to exclude
+    exclude: [
+      'dist/bootstrap.js',
+      'node_modules/*',
+      '**/*.css'
+    ],
 
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
 
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'html'],
 
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+    // web server port
+    port: 9876,
 
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-        plugins: [
-            'karma-jasmine',
-            'karma-chrome-launcher',
-            'karma-jasmine-html-reporter'
-        ]
-    })
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    systemjs: {
+      // Path to your SystemJS configuration file
+      configFile: 'src/system.conf.js',
+
+      // Patterns for files that you want Karma to make available, but not loaded until a module requests them. eg. Third-party libraries.
+      //serveFiles: [
+      //  'dist/app/**/*.js',
+      //],
+
+      //files: [
+      //  'dist/**/*.js',
+      //],
+
+      // SystemJS configuration specifically for tests, added after your config file.
+      // Good for adding test libraries and mock modules
+      config: {
+        paths: {}
+      }
+    },
+
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter',
+      'karma-systemjs'
+    ]
+  })
 }
