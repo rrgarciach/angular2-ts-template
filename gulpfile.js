9237f739a1ba4338fec2a9d0886446ed0d75fd63
rrgarciach <rrgarciach@gmail.com>
2016-02-14 22:12:22 -0600

    Migrated Gulp related styles tasks to its own module.

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,12 @@
   require('./gulp/drakov');
   require('./gulp/typescript');
   require('./gulp/build');
+  require('./gulp/styles');
 
   const runSequence = require('run-sequence');
   const merge = require('merge-stream');
   const del = require('del');
   const sass = require('gulp-sass');
-  const concatCss = require('gulp-concat-css');
-  const minifyCss = require('gulp-minify-css');
   const connect = require('gulp-connect');
   const open = require('gulp-open');
   const KarmaServer = require('karma').Server;
@@ -92,28 +91,6 @@
       'clean-css',
       'dependencies');
     callback();
-  });
-
-  // Compiles SASS files to CSS
-  gulp.task('sass', () => {
-    return gulp.src('src/**/**.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('dist/temp'));
-  });
-
-  // Concatenate all CSS files into one single bundle CSS file
-  gulp.task('css-concat', ['sass'], function () {
-    return gulp.src('dist/temp/**/**.css')
-      .pipe(concatCss("app/assets/css/bundle.css"))
-      .pipe(minifyCss({compatibility: 'ie8'}))
-      .pipe(gulp.dest('dist'));
-  });
-
-
-
-  // Removes temporal CSS
-  gulp.task('clean-css', ['css-concat'], () => {
-    return del(['dist/temp']);
   });
 
 
